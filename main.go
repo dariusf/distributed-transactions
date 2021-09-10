42a83a8e61f8ba376c629f0e99cc615cab31fff8
Darius Foo <darius.foo.tw@gmail.com>
2021-09-10 13:01:46 +0800

    Revert "Profiling"
    This reverts commit 5df33731ca47f2ddbae1bc864c870d26f8b46e03.

--- main.go
+++ main.go
@@ -2,39 +2,10 @@ package main
 
 import (
 	"distributed-transactions/src/dtrans"
-	"fmt"
-	"log"
-	"os"
-	"os/signal"
-	"runtime"
-	"runtime/pprof"
 )
 
-func main1() {
-	dtrans.Start()
-}
-
 func main() {
-	f, err := os.Create(fmt.Sprintf("profile%s.prof", os.Getenv("NODE_ID")))
-	if err != nil {
-		log.Fatal("could not create CPU profile: ", err)
-	}
-	defer f.Close() // error handling omitted for example
-	runtime.SetCPUProfileRate(100000)
-	if err := pprof.StartCPUProfile(f); err != nil {
-		log.Fatal("could not start CPU profile: ", err)
-	}
-
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	go func() {
-		for range c {
-			pprof.StopCPUProfile()
-			os.Exit(0)
-		}
-	}()
-
-	main1()
+	dtrans.Start()
 }
 
 // TODO HANDLE DEADLOCK
